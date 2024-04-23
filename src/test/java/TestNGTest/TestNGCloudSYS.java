557d3e9e88c33a03d15be38343dae870e09374b1
molkacherni <113240691+molkacherni@users.noreply.github.com>
2024-04-23 15:20:17 +0100

    Update TestNGCloudSYS.java

--- src/test/java/TestNGTest/TestNGCloudSYS.java
+++ src/test/java/TestNGTest/TestNGCloudSYS.java
@@ -136,7 +136,7 @@ class NavigationMenuTest {
     }
 
     public void execute() {
-        WebElement gestionDesTiersToggle = waitForElement(By.xpath("(//span[@class='a-TreeView-toggle'])[3]")); 
+        WebElement gestionDesTiersToggle = waitForElement(By.xpath("(//span[@class='a-TreeView-toggle'])[2]")); 
         gestionDesTiersToggle.click();
         driver.findElement(By.partialLinkText("Mes clients")).click();
        
