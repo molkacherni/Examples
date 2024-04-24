dce72b62da24428f98ff228a7c3eca77d7150d44
molkacherni <113240691+molkacherni@users.noreply.github.com>
2024-04-24 15:32:48 +0100

    Update TestNGCloudSYS.java

--- src/test/java/TestNGTest/TestNGCloudSYS.java
+++ src/test/java/TestNGTest/TestNGCloudSYS.java
@@ -18,7 +18,6 @@ import org.openqa.selenium.StaleElementReferenceException;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
-
 import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
