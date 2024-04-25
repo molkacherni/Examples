b35da4e4f0a53bc3b1e719008ab10379a0c39e70
molkacherni <113240691+molkacherni@users.noreply.github.com>
2024-04-25 09:09:15 +0100

    Update TestNGCloudSYS.java

--- src/test/java/TestNGTest/TestNGCloudSYS.java
+++ src/test/java/TestNGTest/TestNGCloudSYS.java
@@ -1,11 +1,10 @@
 package TestNGTest;
 
 import java.text.SimpleDateFormat;
-import java.time.Duration;
 import java.util.Date;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Random;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -14,93 +13,81 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.StaleElementReferenceException;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-
-public class TestNGCloudSYS{
+public class TestNGCloudSYS {
     WebDriver driver;
-    static String randomEmployeeInfo = generateRandomEmployeeInfo();
-    static {
-        randomEmployeeInfo = generateRandomEmployeeInfo();
+    Random random = new Random();
+
+    private String generateTimestamp() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd_HH:mm:ss");
+        String timestamp = dateFormat.format(new Date());
+        int randomNumber = random.nextInt(1000);
+        return timestamp + "_" + randomNumber;
     }
-    
+
     @BeforeTest
     public void setupTest() {
-    	System.out.println(" the random string is : " + randomEmployeeInfo);
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
     }
 
-    @Test(priority = 1)
-    public void testConnexion() {
+    @Test(priority=1)
+    public void testConnexion() throws InterruptedException {
         String timestamp = generateTimestamp();
-        System.out.println("Test Connexion: " + timestamp);
+        System.out.println(" testConnexion: " + timestamp);
         ConnexionTest connexionTest = new ConnexionTest(driver);
         connexionTest.execute();
     }
 
-    @Test(priority = 2)
+    @Test(priority=2)
     public void testNavigationMenu() {
         String timestamp = generateTimestamp();
-        System.out.println("Test Navigation Menu: " + timestamp);
+        System.out.println(" testNavigationMenu: " + timestamp);
         NavigationMenuTest navigationMenuTest = new NavigationMenuTest(driver);
         navigationMenuTest.execute();
     }
 
-    @Test(priority = 3)
+    @Test(priority=3)
     public void testCreerClient() {
         String timestamp = generateTimestamp();
-        System.out.println("Test Creer Client: " + timestamp);
-        CreerClientTest creerClientTest = new CreerClientTest(driver, randomEmployeeInfo);
+        System.out.println(" testCreerClient: " + timestamp);
+        CreerClientTest creerClientTest = new CreerClientTest(driver);
         creerClientTest.execute();
     }
 
-    @Test(priority = 4)
+    @Test(priority=4)
     public void testRechercheClient() {
         String timestamp = generateTimestamp();
-        System.out.println("Test Recherche Client: " + timestamp);
-        String randomVerification = randomEmployeeInfo; // Replace with your random verification string
-        RechercheClientTest rechercheClientTest = new RechercheClientTest(driver, randomEmployeeInfo);
-        rechercheClientTest.execute("CHERNI MOLKA", randomVerification);
+        System.out.println(" testRechercheClient: " + timestamp);
+        RechercheClientTest rechercheClientTest = new RechercheClientTest(driver);
+        rechercheClientTest.execute("CHERNI MOLKA");
 
-        boolean isClientFound = driver.getPageSource().contains(randomEmployeeInfo);
+        boolean isClientFound = driver.getPageSource().contains("CHERNI MOLKA");
         Assert.assertTrue(isClientFound, "Le client recherché n'est pas affiché sur la page.");
     }
 
-    @Test(priority = 5)
+    @Test(priority=5)
     public void testUsernameFieldVisibility() {
         WebDriverWait wait = new WebDriverWait(driver, 10);
+
         WebElement usernameField = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("t_Body_content")));
-        Assert.assertTrue(usernameField.isDisplayed(), "Un nouveau client déjà créé.");
+
+        Assert.assertTrue(usernameField.isDisplayed(), " Un nouveau client déja crée.");
     }
 
     @AfterTest
     public void tearDownTest() {
-        // driver.quit();
+        //driver.quit();
         System.out.println("Test completed successfully");
-    }
-
-    private String generateTimestamp() {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd_HH:mm:ss");
-        return dateFormat.format(new Date());
-    }
-
-    private static String generateRandomEmployeeInfo() {
-        String[] employeeNames = {"CHERNI MOLKA"};
-        Random rand = new Random();
-        String randomEmployeeName = employeeNames[rand.nextInt(employeeNames.length)];
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd_HH:mm:ss");
-        String timestamp = dateFormat.format(new Date());
-        return randomEmployeeName + "_" + timestamp;
     }
 }
 
-class ConnexionTest{
+class ConnexionTest {
     private WebDriver driver;
 
     public ConnexionTest(WebDriver driver) {
@@ -108,14 +95,12 @@ class ConnexionTest{
     }
 
     public void execute() {
-        System.out.println("Executing Connexion Test...");
         driver.get("http://92.205.22.177:8080/apex/f?p=112:LOGIN_DESKTOP::::::");
         WebElement usernameInput = waitForElement(By.id("P101_USERNAME"));
         usernameInput.sendKeys("Molka12");
-      
         WebElement passwordInput = waitForElement(By.id("P101_PASSWORD"));
         passwordInput.sendKeys("0000");
-        WebElement loginButton = waitForElement(By.id("P101_LOGIN"));
+        WebElement loginButton = waitForElement(By.id("P101_LOGIN")); 
         loginButton.click();
     }
 
@@ -133,8 +118,7 @@ class NavigationMenuTest {
     }
 
     public void execute() {
-        System.out.println("Executing Navigation Menu Test...");
-        WebElement gestionDesTiersToggle = waitForElement(By.xpath("(//span[@class='a-TreeView-toggle'])[2]"));
+        WebElement gestionDesTiersToggle = waitForElement(By.xpath("(//span[@class='a-TreeView-toggle'])[3]")); 
         gestionDesTiersToggle.click();
         driver.findElement(By.partialLinkText("Mes clients")).click();
     }
@@ -147,20 +131,25 @@ class NavigationMenuTest {
 
 class CreerClientTest {
     private WebDriver driver;
-    private String employeeInfo;
+    private Random random = new Random();
 
-    public CreerClientTest(WebDriver driver, String employeeInfo) {
+    public CreerClientTest(WebDriver driver) {
         this.driver = driver;
-        this.employeeInfo = employeeInfo;
     }
 
     public void execute() {
-        System.out.println("Executing Creer Client Test...");
         WebElement creerButton = waitForElement(By.id("B166764947412786522"));
         creerButton.click();
         Select selectList = new Select(driver.findElement(By.id("P37_TITRE_CLIENT")));
-        selectList.selectByVisibleText("Madame");
-        driver.findElement(By.id("P37_NOM_CLIENT")).sendKeys(employeeInfo);
+        selectList.selectByVisibleText("Madame"); 
+
+       
+        String randomString = generateRandomString();
+
+        
+        String nomClient = "CHERNI MOLKA_" + randomString;
+        driver.findElement(By.id("P37_NOM_CLIENT")).sendKeys(nomClient);
+
         driver.findElement(By.id("P37_MATRICULE_FISCALE")).sendKeys("03214");
         driver.findElement(By.id("P37_ADRESSE_1")).sendKeys("TUNIS ");
         driver.findElement(By.id("P37_ADRESSE_2")).sendKeys("Résidence zohra,cité khadhra");
@@ -172,6 +161,19 @@ class CreerClientTest {
         creerButton1.click();
     }
 
+    private String generateRandomString() {
+        int length = 5;
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder randomString = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(characters.length());
+            randomString.append(characters.charAt(index));
+        }
+
+        return randomString.toString();
+    }
+
     private WebElement waitForElement(By locator) {
         WebDriverWait wait = new WebDriverWait(driver, 10);
         return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
@@ -180,57 +182,42 @@ class CreerClientTest {
 
 class RechercheClientTest {
     private WebDriver driver;
-    private String rechercheNomClient;
+    private Random random = new Random();
 
-    public RechercheClientTest(WebDriver driver, String rechercheNomClient) {
+    public RechercheClientTest(WebDriver driver) {
         this.driver = driver;
-        this.rechercheNomClient = rechercheNomClient;
     }
 
-    public void execute(String nomClient, String randomVerification) {
-        System.out.println("Executing Recherche Client Test...");
+    public void execute(String nomClient) {
+        String recherche = nomClient  + " " + generateTimestamp();
         WebElement searchBar = waitForElement(By.id("R166763848610786519_search_field"));
-        searchBar.sendKeys(rechercheNomClient);
+        searchBar.sendKeys(recherche);
         searchBar.sendKeys(Keys.RETURN);
-        
-      
-        int maxRetries = 3;
-        int retryCount = 0;
-        boolean isElementDisplayed = false;
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//table[@id='166764050341786521']")));
 
-    
-        while (retryCount < maxRetries && !isElementDisplayed) {
-            WebDriverWait wait = new WebDriverWait(driver, 10); 
-            WebElement tdElement = wait.ignoring(StaleElementReferenceException.class)
-                    .until(driver -> {
-                        WebElement element = driver.findElement(By.xpath("//table[@id='166764050341786521']//td[contains(text(), '" + rechercheNomClient + "')]"));
-                        if (element.isDisplayed()) {
-                            return element;
-                        } else {
-                            return null;
-                        }
-                    });
+        List<WebElement> searchResults = driver.findElements(By.xpath("//table[@id='166764050341786521']/tbody/tr"));
+        if (!searchResults.isEmpty()) {
+            WebElement dernierClient = searchResults.get(searchResults.size() - 1);
+            String ligneComplete = dernierClient.getText();
+            String timestamp = generateTimestamp();
 
+            ligneComplete += " Date de création : " + timestamp;
 
-
-          
-            if (tdElement != null) {
-                isElementDisplayed = true;
-                System.out.println("Client créé.");
-            } else {
-                System.out.println("Aucun résultat trouvé");
-                retryCount++;
-            }
+            System.out.println("Nom du dernier client créé : "  + ligneComplete);
+        } else {
+            System.out.println("Aucun résultat trouvé.");
         }
-
-     
-        Assert.assertTrue(isElementDisplayed, "Le nom du client recherché est pas présent dans le tableau.");
-
     }
-
+    private String generateTimestamp() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd_HH:mm:ss");
+        String timestamp = dateFormat.format(new Date());
+        int randomNumber = random.nextInt(1000);
+        return timestamp + "_" + randomNumber;
+    }
 
     private WebElement waitForElement(By locator) {
         WebDriverWait wait = new WebDriverWait(driver, 10);
         return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
-} 
+}
